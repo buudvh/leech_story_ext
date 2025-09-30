2d40eb99495b8302f278426a3fd9ae46ec42eb16
Dam Van Hoang Buu <buudvh@glotechvn.com>
2025-09-30 10:23:56 +0700

    update idejian

--- idejian/src/search.js
+++ idejian/src/search.js
@@ -1,21 +1,74 @@
+load('libs.js');
 load('config.js');
-function execute(key, page) {
-    let response = fetch(BASE_URL + '/search?keyword=' + key);
+load('common.js');
 
-    if (response.ok) {
-        let doc = response.html();
-        const data = [];
-        doc.select(".rank_ullist li").forEach(e => {
+function execute(key, page) {
+    try {
+        var arrKey = key.split("&");
+        if (!page) page = '1';
+        var sort = '';
+        if (arrKey.length === 2) {
+            sort = arrKey[1];
+        }
+
+        var url = STVHOST + '/io/searchtp/searchBooks/?findinname=' + encodeURIComponent(arrKey[0]) +
+            '&sort=' + sort + '&host=idejian&minc=0&tag=&p=' + page;
+
+        if (key.indexOf("find=") === 0) {
+            url = STVHOST + '/io/searchtp/searchBooks/?find=' + encodeURIComponent(arrKey[0].replace("find=", "")) +
+                '&sort=' + sort + '&host=idejian&minc=0&tag=&p=' + page;
+        }
+
+        var response = fetch(url);
+        if (!response.ok) {
+            return Response.error('fetch ' + url + ' failed: status ' + response.status);
+        }
+
+        var doc = response.html();
+        var next = (parseInt(page, 10) + 1).toString();
+        var el = doc.select("a.booksearch");
+
+        if (!el.length) return null;
+
+        var data = [];
+        el.forEach(function (e) {
+            var stv_story_link = e.select("a").first().attr("href") || "";
+            var parts = stv_story_link.split("/");
+            var bookid = parts.length > 4 ? parts[4] : "";
+
             data.push({
-                cover:e.select("img").first().attr("src"),
-                name: e.select(".rank_bkname a").first().text(),
-                link: BASE_URL + e.select(".rank_bkname a").first().attr("href"),
-                description: e.select(".author").first().text(),
+                name: toCapitalize(e.select(".searchbooktitle").first().text()),
+                link: BASE_URL + '/book/' + bookid + '/',
+                cover: e.select("a:nth-child(1) > img").attr("src") || DEFAULT_COVER,
+                description: e.select("div > span.searchtag").last().text(),
                 host: BASE_URL
-            })
+            });
         });
-        var next = (parseInt(page) + 1).toString();
-        return Response.success(data, next)
+
+        return Response.success(data, next);
+    } catch (error) {
+        return Response.error(error.message);
     }
-    return null;
 }
+
+
+// function execute(key, page) {
+//     let response = fetch(BASE_URL + '/search?keyword=' + key);
+
+//     if (response.ok) {
+//         let doc = response.html();
+//         const data = [];
+//         doc.select(".rank_ullist li").forEach(e => {
+//             data.push({
+//                 cover:e.select("img").first().attr("src"),
+//                 name: e.select(".rank_bkname a").first().text(),
+//                 link: BASE_URL + e.select(".rank_bkname a").first().attr("href"),
+//                 description: e.select(".author").first().text(),
+//                 host: BASE_URL
+//             })
+//         });
+//         var next = (parseInt(page) + 1).toString();
+//         return Response.success(data, next)
+//     }
+//     return null;
+// }
--- idejian/src/toc.js
+++ idejian/src/toc.js
@@ -1,44 +1,35 @@
 load('config.js');
-function execute(url) {
-    let page = 1;
-    url = url.replace(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n?]+)/img, BASE_URL);
-    const match = url.match(/book\/(\d+)/);
-    const bookId = match ? match[1] : null;
-    let data = [];
-    const chapterRegex = /<a href="([^"]+)">([^<]+)<\/a>/g;
-    
-    while(true){
-        url = BASE_URL + 'catelog/' + bookId + '/1/?page=' + page;
-        let response = fetch(url);
-        if (response.ok) {
-            let json = response.json();
-            let html = json.html;
-            if (html.length == 0) {
-                break;
-            }
-            page++;
-            // html = Html.parse(html);
-            // let el = html.select("li a");
-            // for (let i = 0; i < el.size(); i++) {
-            //     var e = el.get(i);
-            //     data.push({
-            //         name: e.text(),
-            //         url: BASE_URL + e.attr("href"),
-            //         host: BASE_URL
-            //     });
-    
-            // }         
-            let match;
-            while ((match = chapterRegex.exec(html)) !== null) {
-                data.push({
-                    url: match[1],
-                    name: match[2].trim(),
-                    host: BASE_URL
-                });
-            }
-        }
-    }
-    
 
-    return Response.success(data);
+function execute(url) {
+    var data = [];
+    var chapterRegex = /<a\s+href="([^"]+)">([^<]+)<\/a>/g;
+    try {
+        var response = fetch(url);
+        if (!response.ok) return Response.success([{
+            url,
+            name: url,
+            host: BASE_URL
+        }]);;
+
+        var json = response.json();
+        var html = json.html;
+        if (html.length === 0) return null;
+
+        var m;
+        while ((m = chapterRegex.exec(html)) !== null) {
+            data.push({
+                url: m[1],
+                name: m[2].trim(),
+                host: BASE_URL
+            });
+        }
+
+        return Response.success(data);
+    } catch (e) {
+        return Response.success([{
+            url: url,
+            name: e.message,
+            host: BASE_URL
+        }]);
+    }
 }
