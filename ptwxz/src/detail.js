e4a5f6394351bcad46ff9ddced5a63056d10d2c6
Dam Van Hoang Buu <buudvh@glotechvn.com>
2025-09-30 15:21:27 +0700

    update

--- ptwxz/src/detail.js
+++ ptwxz/src/detail.js
@@ -11,6 +11,7 @@ function execute(url) {
     let category = $.QA(doc, '#content table table td', {f: x => /类.*别：/.test(x.text()), m: x => x.text().replace(/类.*别：/, '').replace('<br', '').trim(), j: ' '});
     let cover = doc.select('#content table table a > img[align][hspace][vspace]').first().attr('src');
     let description = $.Q(doc, '#content table table div[style]:not([id]):not([onclick])', {remove: 'span, a'}).html();
+    var detail = $.QA(doc, '#content > table > tbody > tr:nth-child(1) > td > table > tbody > tr > td', { m: function (x) { return x.text(); }, j: '<br>' });
     description = cleanHtml(description);
 
     return Response.success({
@@ -18,7 +19,14 @@ function execute(url) {
         cover: cover,
         author: author,
         description: description,
-        detail: String.format('作者: {0}<br>类别: {1}', author, category),
-        host: BASE_URL
+        detail: detail,
+        host: BASE_URL,
+        genres: [
+            {
+                title: category,
+                input: '&update',
+                script: "search.js"
+            }
+        ]
     });
 }
