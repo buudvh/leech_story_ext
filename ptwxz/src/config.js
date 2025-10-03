6e63c2d89ba2a6b06600d2170be33134d6feeedd
Dam Van Hoang Buu <buudvh@glotechvn.com>
2025-10-03 16:56:19 +0700

    update cover

--- ptwxz/src/config.js
+++ ptwxz/src/config.js
@@ -1,6 +1,6 @@
 let BASE_URL = 'https://www.piaotia.com';
 var STVHOST = 'http://14.225.254.182';
-var DEFAULT_COVER = 'https://raw.githubusercontent.com/buudvh/leech_story_ext/main/asset/cover.jpg';
+var DEFAULT_COVER = 'https://raw.githubusercontent.com/buudvh/leech_story_ext/main/asset/cover1.jpg';
 try {
     if (CONFIG_URL) {
         BASE_URL = CONFIG_URL;
