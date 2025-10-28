f159f64b61d93470629801c8fcdd9297b3e9396d
Dam Van Hoang Buu <buudvh@glotechvn.com>
2025-10-28 15:32:31 +0700

    update genre

--- uukanshu/src/genre.js
+++ uukanshu/src/genre.js
@@ -4,6 +4,15 @@ function execute() {
         { title: "聊天群", input: "聊天群", script: "search.js" },
         { title: "万界", input: "万界", script: "search.js" },
         { title: "无限", input: "无限", script: "search.js" },
+        { title: "同时穿越", input: "同时穿越", script: "search.js" },
+
+        //Tu chân liêu thiên quần
+        { title: "修真聊天群", input: "修真聊天群", script: "search.js" },
+        { title: "霸宋", input: "霸宋", script: "search.js" },
+        { title: "宋书航", input: "宋书航", script: "search.js" },
+
+        //Ngã cật tây hồng thị
+        { title: "盘龙", input: "盘龙", script: "search.js" },
 
         //Thần Đông
         { title: "神墓", input: "神墓", script: "search.js" },
@@ -22,7 +31,14 @@ function execute() {
         { title: "轮回", input: "轮回", script: "search.js" },
         { title: "庆余年", input: "庆余年", script: "search.js" },
         { title: "斗罗", input: "斗罗", script: "search.js" },
+        { title: "武魂", input: "武魂", script: "search.js" },
+
+        //Đấu phá
         { title: "斗破", input: "斗破", script: "search.js" },
+        { title: "武动", input: "武动", script: "search.js" },
+        { title: "大主宰", input: "大主宰", script: "search.js" },
+        { title: "元尊", input: "元尊", script: "search.js" },
+        
         { title: "妖神记", input: "妖神记", script: "search.js" },
         { title: "师兄啊", input: "师兄啊", script: "search.js" },
         { title: "全職法師", input: "全職法師", script: "search.js" },
@@ -32,10 +48,23 @@ function execute() {
         { title: "万族之劫", input: "万族之劫", script: "search.js" },
         { title: "星门", input: "星门", script: "search.js" },
 
+        //Kiếm đạo độc tôn
+        { title: "剑道独尊", input: "剑道独尊", script: "search.js" },
+
         //Harry Potter
         { title: "哈利波特", input: "哈利波特", script: "search.js" },
+        { title: "霍格沃兹", input: "霍格沃兹", script: "search.js" },
+
+        //Comic
+        { title: "美漫", input: "美漫", script: "search.js" },
+        { title: "漫威", input: "漫威", script: "search.js" },
+
+        //Trung mạn
+        { title: "磁场转动", input: "磁场转动", script: "search.js" },
 
         //Anime
+        { title: "宝可梦", input: "宝可梦", script: "search.js" },
+        { title: "综漫", input: "综漫", script: "search.js" },
         { title: "龙珠", input: "龙珠", script: "search.js" },
         { title: "火影", input: "火影", script: "search.js" },
         { title: "木叶", input: "火影", script: "search.js" },
@@ -48,6 +77,7 @@ function execute() {
         { title: "骑士", input: "骑士", script: "search.js" },
         { title: "死神", input: "死神", script: "search.js" },
         { title: "全職獵人", input: "全職獵人", script: "search.js" },
+        { title: "鬼灭之刃", input: "鬼灭之刃", script: "search.js" },
         
         { title: "動漫同人", input: "https://uukanshu.cc/class_9_{0}.html", script: "gen.js" },
         { title: "玄幻奇幻", input: "https://uukanshu.cc/class_1_{0}.html", script: "gen.js" },
