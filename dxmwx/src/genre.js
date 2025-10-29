7de6a3ffda4e76c1288b6c154a32993b0aeccf67
Dam Van Hoang Buu <buudvh@glotechvn.com>
2025-10-29 13:55:03 +0700

    update genre

--- dxmwx/src/genre.js
+++ dxmwx/src/genre.js
@@ -4,7 +4,8 @@ function execute() {
         { title: "聊天群", input: "聊天群", script: "search.js" },
         { title: "万界", input: "万界", script: "search.js" },
         { title: "无限", input: "无限", script: "search.js" },
-        { title: "同时穿越", input: "同时穿越", script: "search.js" },
+        { title: "同时穿越", input: "同时穿越", script: "search.js" }, //Đồng thời xuyên qua
+        { title: "轮回", input: "轮回", script: "search.js" }, //Luân Hồi
 
         //Tu chân liêu thiên quần
         { title: "修真聊天群", input: "修真聊天群", script: "search.js" },
@@ -12,72 +13,82 @@ function execute() {
         { title: "宋书航", input: "宋书航", script: "search.js" },
 
         //Ngã cật tây hồng thị
-        { title: "盘龙", input: "盘龙", script: "search.js" },
+        { title: "盘龙", input: "盘龙", script: "search.js" }, //Bàn Long
+        { title: "吞噬", input: "吞噬", script: "search.js" }, //Thôn Phệ
+        { title: "星辰变", input: "星辰变", script: "search.js" }, //Tinh thần biến
+
+        //Trạch Trư
+        { title: "牧神记", input: "牧神记", script: "search.js" }, //Mục thần ký
+        { title: "帝尊", input: "帝尊", script: "search.js" }, //Đế Tôn
+        { title: "魔狱玄胎", input: "魔狱玄胎", script: "search.js" }, //Ma ngục huyền thai
 
         //Thần Đông
-        { title: "神墓", input: "神墓", script: "search.js" },
-        { title: "遮天", input: "遮天", script: "search.js" },
-        { title: "完美世界", input: "完美世界", script: "search.js" },
-        { title: "圣墟", input: "圣墟", script: "search.js" },
+        { title: "神墓", input: "神墓", script: "search.js" }, //Thần Mộ
+        { title: "遮天", input: "遮天", script: "search.js" }, //Già Thiên
+        { title: "完美世界", input: "完美世界", script: "search.js" }, //Hoàn Mỹ Thế Giới
+        { title: "圣墟", input: "圣墟", script: "search.js" }, //Thánh Khư
 
-        { title: "吞噬", input: "吞噬", script: "search.js" },
-        { title: "凡人", input: "凡人", script: "search.js" },
+        { title: "凡人", input: "凡人", script: "search.js" }, //Phàm Nhân
 
-        { title: "一世之尊", input: "一世之尊", script: "search.js" },
-        { title: "诡秘", input: "诡秘", script: "search.js" },
+        //Lão mực
+        { title: "一世之尊", input: "一世之尊", script: "search.js" }, //Nhất thế chi tôn
+        { title: "孟奇", input: "孟奇", script: "search.js" }, //Mạnh Kỳ
+        { title: "诡秘", input: "诡秘", script: "search.js" }, //Quỷ bí
 
-        { title: "西游", input: "西游", script: "search.js" },
-        { title: "大奉", input: "大奉", script: "search.js" },
-        { title: "轮回", input: "轮回", script: "search.js" },
-        { title: "庆余年", input: "庆余年", script: "search.js" },
-        { title: "斗罗", input: "斗罗", script: "search.js" },
-        { title: "武魂", input: "武魂", script: "search.js" },
+        //Mộng nhập
+        { title: "永生", input: "永生", script: "search.js" }, //Vĩnh sinh
+        { title: "阳神", input: "阳神", script: "search.js" }, //Dương Thần
+
+        { title: "西游", input: "西游", script: "search.js" }, //Tây du
+        { title: "大奉", input: "大奉", script: "search.js" }, //Đại Phụng
+        { title: "庆余年", input: "庆余年", script: "search.js" }, //Khánh dư niên
+        { title: "斗罗", input: "斗罗", script: "search.js" }, //Đấu la
+        { title: "武魂", input: "武魂", script: "search.js" }, //Võ hồn
 
         //Đấu phá
-        { title: "斗破", input: "斗破", script: "search.js" },
-        { title: "武动", input: "武动", script: "search.js" },
-        { title: "大主宰", input: "大主宰", script: "search.js" },
-        { title: "元尊", input: "元尊", script: "search.js" },
+        { title: "斗破", input: "斗破", script: "search.js" }, //Đấu phá
+        { title: "武动", input: "武动", script: "search.js" }, //Vũ động
+        { title: "大主宰", input: "大主宰", script: "search.js" }, //Đại chúa tể
+        { title: "元尊", input: "元尊", script: "search.js" }, //Nguyên tôn
         
-        { title: "妖神记", input: "妖神记", script: "search.js" },
-        { title: "师兄啊", input: "师兄啊", script: "search.js" },
-        { title: "全職法師", input: "全職法師", script: "search.js" },
-        { title: "剑来", input: "剑来", script: "search.js" },
-        { title: "雪中", input: "雪中", script: "search.js" },
-        { title: "全球高武", input: "全球高武", script: "search.js" },
-        { title: "万族之劫", input: "万族之劫", script: "search.js" },
-        { title: "星门", input: "星门", script: "search.js" },
+        { title: "妖神记", input: "妖神记", script: "search.js" }, //Yêu thần ký
+        { title: "师兄啊", input: "师兄啊", script: "search.js" }, //Sư huynh a
+        { title: "全職法師", input: "全職法師", script: "search.js" }, //Toàn Chức Pháp Sư
+        { title: "剑来", input: "剑来", script: "search.js" }, //Kiếm lai
+        { title: "雪中", input: "雪中", script: "search.js" }, //Tuyết trung
 
-        //Kiếm đạo độc tôn
-        { title: "剑道独尊", input: "剑道独尊", script: "search.js" },
+        { title: "全球高武", input: "全球高武", script: "search.js" }, //Toàn Cầu Cao Võ
+        { title: "万族之劫", input: "万族之劫", script: "search.js" }, //Vạn Tộc Chi Kiếp
+        { title: "星门", input: "星门", script: "search.js" }, //Tinh Môn
 
-        //Harry Potter
-        { title: "哈利波特", input: "哈利波特", script: "search.js" },
-        { title: "霍格沃兹", input: "霍格沃兹", script: "search.js" },
+        { title: "剑道独尊", input: "剑道独尊", script: "search.js" }, //Kiếm đạo độc tôn
+
+        { title: "哈利波特", input: "哈利波特", script: "search.js" }, //Harry Potter
+        { title: "霍格沃兹", input: "霍格沃兹", script: "search.js" }, //Hogwart
 
         //Comic
-        { title: "美漫", input: "美漫", script: "search.js" },
-        { title: "漫威", input: "漫威", script: "search.js" },
+        { title: "美漫", input: "美漫", script: "search.js" }, //Comic
+        { title: "漫威", input: "漫威", script: "search.js" }, //Marvel
 
         //Trung mạn
-        { title: "磁场转动", input: "磁场转动", script: "search.js" },
+        { title: "磁场转动", input: "磁场转动", script: "search.js" }, //Từ Trường Chuyển Động
 
         //Anime
-        { title: "宝可梦", input: "宝可梦", script: "search.js" },
-        { title: "综漫", input: "综漫", script: "search.js" },
-        { title: "龙珠", input: "龙珠", script: "search.js" },
-        { title: "火影", input: "火影", script: "search.js" },
-        { title: "木叶", input: "火影", script: "search.js" },
-        { title: "鸣人", input: "鸣人", script: "search.js" },
-        { title: "犬夜叉", input: "犬夜叉", script: "search.js" },
-        { title: "海贼", input: "海贼", script: "search.js" },
-        { title: "妖尾", input: "妖尾", script: "search.js" },
-        { title: "奥特曼", input: "奥特曼", script: "search.js" },
-        { title: "假面骑士", input: "假面骑士", script: "search.js" },
-        { title: "骑士", input: "骑士", script: "search.js" },
-        { title: "死神", input: "死神", script: "search.js" },
-        { title: "全職獵人", input: "全職獵人", script: "search.js" },
-        { title: "鬼灭之刃", input: "鬼灭之刃", script: "search.js" },
+        { title: "宝可梦", input: "宝可梦", script: "search.js" }, //Pokemon
+        { title: "综漫", input: "综漫", script: "search.js" }, //Tổng mạn
+        { title: "龙珠", input: "龙珠", script: "search.js" }, //Long châu
+        { title: "火影", input: "火影", script: "search.js" }, //Hokage
+        { title: "木叶", input: "火影", script: "search.js" }, //Konoha
+        { title: "鸣人", input: "鸣人", script: "search.js" }, //Naruto
+        { title: "犬夜叉", input: "犬夜叉", script: "search.js" }, //Inuyasha
+        { title: "海贼", input: "海贼", script: "search.js" }, //Once piece
+        { title: "妖尾", input: "妖尾", script: "search.js" }, //Fairy tail
+        { title: "奥特曼", input: "奥特曼", script: "search.js" }, //Ultraman
+        { title: "假面骑士", input: "假面骑士", script: "search.js" }, //Kamen rider
+        { title: "骑士", input: "骑士", script: "search.js" }, //Rider
+        { title: "死神", input: "死神", script: "search.js" }, //Bleach
+        { title: "全職獵人", input: "全職獵人", script: "search.js" }, //HunterxHunter
+        { title: "鬼灭之刃", input: "鬼灭之刃", script: "search.js" }, //Kimetsu no Yaiba
         
         { "title": "玄幻", "input": "/list/玄幻.html", "script": "gen.js" },
         { "title": "奇幻", "input": "/list/奇幻.html", "script": "gen.js" },
