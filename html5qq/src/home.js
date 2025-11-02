801df6a329b08f18cb7695050add602e2955fb78
buudvh <damhoangbuu@gmail.com>
2025-11-02 17:42:36 +0700

    update

--- html5qq/src/home.js
+++ html5qq/src/home.js
@@ -3,6 +3,8 @@ function execute() {
         { title:"全部分类",input:"https://bookshelf.html5.qq.com/qbread/api/rank/list?ch=001995&groupid=1501&start={{page}}&count=20&sort=0&sub=&tag=&words=&finish=&t=20230302222523", script: "gen.js"},
         { title: "诸天流", input: "groupid=20023&start={{page}}&count=20&sort=0&sub=", script: "gen.js" },
         { title: "无限流", input: "groupid=20008&start={{page}}&count=20&sort=0&sub=", script: "gen.js" },
+        { title: "轮回者", input: "groupid=20261&start={{page}}&count=20&sort=0&sub=", script: "gen.js" },
+        { title: "衍生同人", input: "groupid=1512&start={{page}}&count=20&sort=0&sub=衍生同人", script: "gen.js" },
         { title: "武侠同人", input: "groupid=1503&start={{page}}&count=20&sort=0&sub=武侠同人", script: "gen.js" },
         { title: "玄幻", input: "groupid=1501&start={{page}}&count=20&sort=0&sub=", script: "gen.js" },
         { title: "仙侠", input: "groupid=1504&start={{page}}&count=20&sort=0&sub=", script: "gen.js" },
