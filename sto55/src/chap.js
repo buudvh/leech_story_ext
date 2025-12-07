0e7cedac1103f3b5ddb301c422e32d55e5155c0a
buudvh <damhoangbuu@gmail.com>
2025-12-07 15:00:41 +0700

    update

--- sto55/src/chap.js
+++ sto55/src/chap.js
@@ -28,22 +28,79 @@ function execute(url) {
     }
 }
 
+// function removeSto55Lines(html) {
+//     // Tách thành từng dòng theo <br>
+//     const lines = html.split(/<br\s*\/?>/i);
+
+//     // Chuẩn hóa toàn bộ ký tự unicode về dạng "thường dân" nhất
+//     const normalize = s => s.normalize("NFKD").replace(/[\u0300-\u036f]/g, "");
+
+//     // Regex tìm "sto55.com" sau khi normalize
+//     const pattern = /sto\s*55\s*\.?\s*com/i;
+
+//     // Giữ lại các dòng không chứa biến thể sto55.com
+//     const filtered = lines.filter(line => {
+//         const norm = normalize(line).replace(/[^\x00-\x7F]/g, "");
+//         return !pattern.test(norm);
+//     });
+
+//     // Ghép lại thành HTML với <br>
+//     return filtered.join("<br>");
+// }
+
 function removeSto55Lines(html) {
-    // Tách thành từng dòng theo <br>
-    const lines = html.split(/<br\s*\/?>/i);
+    var lines = html.split(/<br\s*\/?>/i);
 
-    // Chuẩn hóa toàn bộ ký tự unicode về dạng "thường dân" nhất
-    const normalize = s => s.normalize("NFKD").replace(/[\u0300-\u036f]/g, "");
+    // Bảng chuyển Unicode → ASCII
+    var map = {
+        // s
+        'ѕ': 's', '𝑠': 's', '𝗌': 's', '𝘀': 's', '𝓈': 's', '𝘴': 's',
+        '𝒮': 's', '𝐬': 's', '𝐒': 's', '𝕤': 's', '𝕊': 's',
 
-    // Regex tìm "sto55.com" sau khi normalize
-    const pattern = /sto\s*55\s*\.?\s*com/i;
+        // t
+        'τ': 't', 'ᴛ': 't', '𝘁': 't', '𝓉': 't', '𝐭': 't', '𝐓': 't',
+        '𝑡': 't', '𝒯': 't', '𝕥': 't', '𝕋': 't',
 
-    // Giữ lại các dòng không chứa biến thể sto55.com
-    const filtered = lines.filter(line => {
-        const norm = normalize(line).replace(/[^\x00-\x7F]/g, "");
-        return !pattern.test(norm);
-    });
+        // o
+        'о': 'o', '𝑜': 'o', '𝗈': 'o', '𝘰': 'o', '𝓸': 'o', '𝒪': 'o',
+        '𝐨': 'o', '𝐎': 'o', '𝕠': 'o', '𝕆': 'o',
 
-    // Ghép lại thành HTML với <br>
+        // 5
+        '𝟝': '5', '𝟧': '5', '𝟻': '5',
+        '⓹': '5', '➄': '5', '➎': '5',
+
+        // dấu chấm
+        '．': '.',
+
+        // c
+        'ᴄ': 'c', '𝒞': 'c', '𝐜': 'c', '𝐂': 'c', '𝕔': 'c', '𝕮': 'c',
+
+        // m
+        'ᴍ': 'm', '𝗺': 'm', '𝘮': 'm', '𝓶': 'm', '𝐦': 'm', '𝐌': 'm'
+    };
+
+    function normalize(str) {
+        if (str.normalize) {
+            str = str.normalize("NFKD");
+        }
+        var out = "";
+        for (var i = 0; i < str.length; i++) {
+            var ch = str.charAt(i);
+            out += map[ch] ? map[ch] : ch;
+        }
+        return out;
+    }
+
+    // Regex nhận dạng sto55.com sau khi normalize
+    var pattern = /s\s*t\s*o\s*5\s*5\s*\.?\s*c\s*o\s*m/i;
+
+    var filtered = [];
+    for (var i = 0; i < lines.length; i++) {
+        var norm = normalize(lines[i]);
+        if (!pattern.test(norm)) {
+            filtered.push(lines[i]);
+        }
+    }
+
     return filtered.join("<br>");
 }
--- twkan/src/chap.js
+++ twkan/src/chap.js
@@ -11,8 +11,10 @@ function execute(url) {
         htm.select("div").remove();
         htm.select("a").remove();
         htm.select("h1").remove();
+        htm.select("script").remove();
 
         htm = htm.html();
+        htm = removeTwkanLines(htm);
         htm = cleanHtml(htm)
             .replace(/^第\d+章.*?<br>/, '') // Ex: '  第11745章 大结局，终<br>'
             .replace('(本章完)', '');
@@ -22,3 +24,42 @@ function execute(url) {
         return Response.error('fetch ' + url + ' failed: ' + error.message);
     }
 }
+
+function removeTwkanLines(html) {
+    // Tách từng dòng theo <br>
+    var lines = html.split(/<br\s*\/?>/i);
+
+    // Bảng chuyển ký tự Latin mở rộng → ASCII
+    var map = {
+        'ᴛ': 't', 'ᴡ': 'w', 'ᴋ': 'k', 'ᴀ': 'a', 'ɴ': 'n',
+        'ᴄ': 'c', 'ᴏ': 'o', 'ᴍ': 'm'
+    };
+
+    function normalize(str) {
+        // NFKD chỉ hoạt động khi môi trường hỗ trợ normalize; nếu không, giữ nguyên
+        if (str.normalize) {
+            str = str.normalize("NFKD");
+        }
+
+        var result = "";
+        for (var i = 0; i < str.length; i++) {
+            var ch = str.charAt(i);
+            result += map[ch] ? map[ch] : ch;
+        }
+        return result;
+    }
+
+    // Regex nhận dạng TWKAN
+    var pattern = /t\s*w\s*k\s*a\s*n/i;
+
+    var filtered = [];
+    for (var i = 0; i < lines.length; i++) {
+        var norm = normalize(lines[i]);
+        if (!pattern.test(norm)) {
+            filtered.push(lines[i]);
+        }
+    }
+
+    return filtered.join("<br>");
+}
+
