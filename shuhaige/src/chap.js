e5b984ae529627a27f47419a5f5daf237fea9e63
buudvh <damhoangbuu@gmail.com>
2025-12-17 21:36:47 +0700

    fix(shuhaige): - fix lỗi không lấy hết content chap

--- shuhaige/src/chap.js
+++ shuhaige/src/chap.js
@@ -3,20 +3,37 @@ load('libs.js');
 
 function execute(url) {
     try {
-        var response = crawler.get(url);
-        if (!response.ok) throw new Error(`Status ${response.status}`)
-        
-        var doc = response.html();
-        var htm = doc.select("#content");
-        htm.select("div").remove();
-        htm.select("a").remove();
-        htm.select("h1").remove();
-
-        htm = htm.html();
-        htm = htm.cleanHtml();
-
+        var htm = "";
+        do {
+            var content;
+            [content, url] = getContentPaper(url);
+            htm += content;
+        } while (url != null);
         return Response.success(htm);
     } catch (error) {
         return Response.error(`Url ${url} \nMessage: ${error.message}`);
     }
+}
+
+function getNextContent(doc) {
+    var nextElm = doc.select("#pager_next");
+    if (nextElm && nextElm.text() == ">下一页") return nextElm.attr("href").mayBeFillHost(BASE_URL);
+    return null;
+}
+
+function getContentPaper(url) {
+    var response = crawler.get(url);
+    if (!response.ok) throw new Error(`Status ${response.status}`)
+
+    var doc = response.html();
+    var htm = doc.select("#content");
+    htm.select("div").remove();
+    htm.select("a").remove();
+    htm.select("h1").remove();
+
+    htm = htm.html();
+    htm = htm.replace(/<p[^>]*>[^<]*(?:下一页|shuhaige)[^<]*<\/p>/gi, '');
+    htm = htm.cleanHtml();
+
+    return [htm, getNextContent(doc)];
 }
