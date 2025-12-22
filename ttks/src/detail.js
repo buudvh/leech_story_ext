2cd01930fd107b78862050c2b4a964fce53cd378
Dam Van Hoang Buu <buudvh@glotechvn.com>
2025-12-22 11:51:56 +0700

    update

--- ttks/src/detail.js
+++ ttks/src/detail.js
@@ -11,19 +11,29 @@ function execute(url) {
         var bookName = doc.select('#__layout > div > div:nth-child(2) > div > div.pure-g.novel_info > div.pure-u-xl-5-6.pure-u-lg-5-6.pure-u-md-2-3.pure-u-1-2 > ul > li:nth-child(1) > h1').text();
         var cover = doc.select("#__layout > div > div:nth-child(2) > div > div.pure-g.novel_info > div.pure-u-xl-1-6.pure-u-lg-1-6.pure-u-md-1-3.pure-u-1-2 > a > amp-img").first().attr("src") || DEFAULT_COVER;
         var genreName = doc.select('#__layout > div > div:nth-child(2) > div > div.pure-g.novel_info > div.pure-u-xl-5-6.pure-u-lg-5-6.pure-u-md-2-3.pure-u-1-2 > ul > li:nth-child(3)').text().trim().replace('類別：', '');
-        var genres = DEFAULT_GENRES.find(p => genreName.indexOf(p.title) != -1);
+        var genres = DEFAULT_GENRES.find(p => p.title == genreName);
+        var recommendBooks = [];
+        (doc.select("#__layout > div > div:nth-child(2) > div > div.more_recommend > a") || []).forEach(e => {
+            recommendBooks.push({
+                name: e.text().convertT2S(),
+                link: e.attr("href"),
+                host: BASE_URL,
+            })
+        });
 
         return Response.success({
             name: bookName.formatTocName(),
             author: authorElm.text().convertT2S(),
             cover: cover,
             description: doc.select("#__layout > div > div:nth-child(2) > div > div.description > div > p").text().convertT2S(),
+            detail: "Last chapter: " + doc.select("head > meta[name=og:novel:latest_chapter_name]").attr("content"),
             host: BASE_URL,
+            ongoing: doc.select("head > meta[name=og:novel:status]").attr("content") == "連載",
             suggests: [
                 {
-                    title: "同作者",
-                    input: authorElm.text(),
-                    script: "search.js"
+                    title: "Recommend",
+                    input: JSON.stringify(recommendBooks),
+                    script: "recommend.js"
                 }
             ],
             genres: [
@@ -31,6 +41,11 @@ function execute(url) {
                     title: genreName || "全部分类",
                     input: genres != undefined ? genres.input : DEFAULT_GENRES[0].input,
                     script: "gen.js"
+                },
+                {
+                    title: authorElm.text(),
+                    input: authorElm.text(),
+                    script: "search.js"
                 }
             ],
             comments: [
--- ttks/src/home.js
+++ ttks/src/home.js
@@ -1,16 +1,16 @@
 function execute() {
     return Response.success([
-        {"title": "連載", "input": "/novel/class/lianzai/{0}.html", "script": "gen.js"},
+        {"title": "Update", "input": "/novel/class/lianzai/{0}.html", "script": "gen.js"},
+        {"title": "同人", "input": "/novel/class/tongren/{0}.html", "script": "gen.js"},
         {"title": "玄幻", "input": "/novel/class/xuanhuan/{0}.html", "script": "gen.js"},
-        {"title": "都市", "input": "/novel/class/dushi/{0}.html", "script": "gen.js"},
+        {"title": "其它", "input": "/novel/class/qita/{0}.html", "script": "gen.js"},
         {"title": "仙俠", "input": "/novel/class/wuxia/{0}.html", "script": "gen.js"},
+        {"title": "科幻", "input": "/novel/class/kehuan/{0}.html", "script": "gen.js"},
+        {"title": "都市", "input": "/novel/class/dushi/{0}.html", "script": "gen.js"},
         {"title": "言情", "input": "/novel/class/yaniqng/{0}.html", "script": "gen.js"},
         {"title": "遊戲", "input": "/novel/class/youxi/{0}.html", "script": "gen.js"},
-        {"title": "科幻", "input": "/novel/class/kehuan/{0}.html", "script": "gen.js"},
         {"title": "懸疑", "input": "/novel/class/kongbu/{0}.html", "script": "gen.js"},
         {"title": "靈異", "input": "/novel/class/xuanhuan/{0}.html", "script": "gen.js"},
         {"title": "軍事", "input": "/novel/class/lishi/{0}.html", "script": "gen.js"},
-        {"title": "現言", "input": "/novel/class/tongren/{0}.html", "script": "gen.js"},
-        {"title": "其它", "input": "/novel/class/qita/{0}.html", "script": "gen.js"}
     ]);
 }
