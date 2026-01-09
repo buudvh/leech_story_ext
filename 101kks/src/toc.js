86b457e9f35c0c458fee733ffa67de6a488aa8fc
Dam Van Hoang Buu <buudvh@glotechvn.com>
2026-01-09 12:30:54 +0700

    update

--- 101kks/src/toc.js
+++ 101kks/src/toc.js
@@ -5,12 +5,27 @@ function execute(url) {
     try {
         url = url.replace(/\/book\/(\d+)\.html/, "/ajax_novels/chapterlist/$1.html");
         var response = crawler.get(url);
-        if (!response.ok) throw new Error(`Status ${response.status}`);
+        var data = [];
+        if (response.status == 403) {
+            var browser = Engine.newBrowser(); // Khởi tạo browser
+            browser.launch(url, 10000); // Mở trang web với timeout, trả về Document object
+            var doc = browser.html(); // Trả về Document object của trang web
+            browser.close(); // Đóng browser khi đã xử lý xong
+            var elms = doc.select("a");
+            if (!elms.length) throw new Error("Length = 0");
+            elms.forEach(e => {
+                data.push({
+                    name: e.text().formatTocName(),
+                    url: e.attr("href"),
+                    host: BASE_URL,
+                });
+            });
+            return Response.success(data);
+        } else if (!response.ok) throw new Error(`Status ${response.status}`);
 
         var html = response.text();
         var regex = /href="([^"]+)">([^<]+)/g;
         var match;
-        var data = [];
 
         while ((match = regex.exec(html)) !== null) {
             data.push({
