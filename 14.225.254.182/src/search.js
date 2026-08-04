37305d334bf5a08305512f33fac0d75fa35dfdd2
Dam Van Hoang Buu <buudvh@glotechvn.com>
2026-08-04 11:18:49 +0700

    update

--- 14.225.254.182/src/search.js
+++ 14.225.254.182/src/search.js
@@ -1,29 +1,34 @@
 function execute(query, page) {
-    var pageInt = parseInt(page) || 1;
-    var baseUrl = (typeof BASE_URL !== 'undefined') ? BASE_URL : "http://14.225.254.182";
+    var url = '';
+    try {
+        var pageInt = parseInt(page) || 1;
+        var baseUrl = (typeof BASE_URL !== 'undefined') ? BASE_URL : "http://14.225.254.182";
 
-    var url = baseUrl + '/search/?find=&findinname=' + encodeURIComponent(query) + '&minc=0&sort=update&tag=&p=' + page;
-    var response = fetch(url);
+        var url = baseUrl + '/search/?find=&findinname=' + encodeURIComponent(query) + '&minc=0&sort=update&tag=&p=' + page;
+        var response = fetch(url);
 
-    if (!response.ok) return Response.error('fetch ' + url + ' failed: status ' + response.status);
+        if (!response.ok) throw new Error(`Status = ${response.status}`);
 
-    var doc = response.html();
-    var next = (parseInt(page, 10) + 1).toString();
-    var el = doc.select("a.booksearch");
+        var doc = response.html();
+        var next = (parseInt(page, 10) + 1).toString();
+        var el = doc.select("a.booksearch");
 
-    if (!el.length) return null;
+        if (!el.length) return null;
 
-    var data = [];
-    el.forEach(function (e) {
-        data.push({
-            name: e.select(".searchbooktitle").first().text(),
-            link: e.select("a").first().attr("href"),
-            cover: e.select("img").attr("src"),
-            description: e.select("div > span.searchtag").first().text() + "|" + e.select("div > span.searchbookauthor").first().text()
-                + "\n" + e.select("div > span.lhr").last().text(),
-            host: baseUrl
+        var data = [];
+        el.forEach(function (e) {
+            data.push({
+                name: e.select(".searchbooktitle").first().text(),
+                link: e.select("a").first().attr("href"),
+                cover: e.select("img").attr("src"),
+                description: e.select("div > span.searchtag").first().text() + "|" + e.select("div > span.searchbookauthor").first().text()
+                    + "\n" + e.select("div > span.lhr").last().text(),
+                host: baseUrl
+            });
         });
-    });
 
-    return Response.success(data, next);
+        return Response.success(data, next);
+    } catch (error) {
+        return Response.error(`Url: ${url}\nMessage: ${error.message}`);
+    }
 }
